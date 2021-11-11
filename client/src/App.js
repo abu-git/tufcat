970897324fb96813f8e16b8becb3fa84a29c8c68
abu-git <abu.sammie@gmail.com>
2021-11-11 17:08:40 +0200

    navbar started

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 //import './App.css';
 import Intro from "./components/Intro";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
       <Intro />
+      <Navigation />
     </div>
   );
 }
---
+++ client/src/components/Navigation.js
@@ -0,0 +1,29 @@
+import { Container } from '@mui/material'
+import  { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles({
+    nav: {
+        position: "sticky",
+        top: "0",
+        display: "flex",
+        justifyContent: "space-around",
+        padding: "1em 0",
+        background: "#fab246"
+    }
+})
+
+export default function Navigation(){
+    const classes = useStyles()
+    return(
+        <>
+            <Container maxWidth="xl">
+                <nav className={classes.nav}>
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+                    <a href="#">Admin Login</a>
+                </nav>
+            </Container>
+        </>
+    )
+}
