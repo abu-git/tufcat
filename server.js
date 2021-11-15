a9868f0ea55a4fcabe26e08a95a413de605e91f5
abu-git <abu.sammie@gmail.com>
2021-11-15 19:17:05 +0200

    another helmet content security fix try

--- server.js
+++ server.js
@@ -7,18 +7,7 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.use(helmet({
-	contentSecurityPolicy: {
-		directives: {
-			defaultSrc: ["'self'"],
-			objectSrc: ["'none'"],
-			scriptSrc: ["'self'", "unpkg.com", "polyfill.io"],
-			styleSrc: ["'self'", "https: 'unsafe-inline'"],
-			upgradeInsecureRequests: [],
-            imgSrc: ["'self'", "data:"],
-		},
-	},
-}))
+app.use(helmet({ contentSecurityPolicy: false }))
 
 app.use(cors())
 
